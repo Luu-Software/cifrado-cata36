9c4dd9f5a3fe367280d65ef50bcd1dc3d09b7e21
cata36 <cataroitman@gmail.com>
2026-06-09 15:14:32 -0300

    termine!

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { preguntar } from './lib/consola.ts';
 import { cifrar, descifrar } from './lib/funcionesCriptográficas.ts';
 
-let palabra: string = ''; // Preguntar por la palabra a cifrar o descifrar
-let claveTexto: string = ''; // Preguntar por la clave de cifrado (número)
-let accion: string = ''; // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
 
-let resultado: string = ''; // COMPLETAR
+let palabra: string = preguntar("Ingresa una palabra:");
+let claveTexto: string = preguntar('Ingresa la clave:'); // Preguntar por la clave de cifrado (número)
+let accion: string = preguntar('¿Que queres hacer? (cifrar/descifrar)'); // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
+
+
+let resultado: string = '';
+
+
+
+
+if (accion === "cifrar"){
+    resultado = cifrar(palabra,Number(claveTexto));
+}
+else if(accion === "descifrar"){
+resultado = descifrar(palabra,Number(claveTexto));
+
+
+}
+
+
+
+
+
+
 console.log(resultado);
+
+
--- src/manija.ts
+++ src/manija.ts
@@ -1,10 +1,28 @@
 import { preguntar } from './lib/consola.ts';
 
+
 function obtenerPrefijo(numero: number, tamañoPrefijo: number): number {
-  return 0; // COMPLETAR
+  return numero / Math.pow(10, 16-tamañoPrefijo);
 }
 
-let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
 
-let red: string = ''; // COMPLETAR
+let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+let prefijo2
+let prefijo1
+prefijo2 = obtenerPrefijo(Number(tarjetaTexto),2)
+prefijo1 = obtenerPrefijo(Number(tarjetaTexto),1)
+let red: string = '';
+
+
+if(prefijo2 === 34 || prefijo2 === 37){
+  red = "AMEX"
+} else if (prefijo1 === 4){
+  red = "VISA"
+} else if (prefijo2 === 51 || prefijo2 === 52 || prefijo2 === 53 || prefijo2 === 54 || prefijo2 === 55) {
+  red = "MASTERCARD"
+} else {
+red = "DESCONOCIDO"
+}
+
+
 console.log(red);
